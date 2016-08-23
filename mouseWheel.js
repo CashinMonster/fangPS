f620e06535345df634f39331fdde8874d8f1028b
Amos619 <913739886@qq.com>
2016-08-23 10:11:11 +0800

    add

---
+++ mouseWheel.js
@@ -0,0 +1,29 @@
+function mouseWheel(obj,fn){
+		var ff = window.navigator.userAgent.indexOf("Firefox");
+//		alert(ff);
+		if (ff!=-1){
+			//火狐
+			obj.addEventListener("DOMMouseScroll",wheel,false);
+		}else{
+			obj.onmousewheel = wheel;
+		}
+		
+		function wheel(ev){
+			var ev = ev||window.event;
+			var down = true;//
+			if (ev.detail){
+				//火狐
+				down = ev.detail<0;
+			}else{
+				down = ev.wheelDelta>0;
+			}
+			//Function.call(对象,down,ev),apply(对象,数组);
+//			fn(down);
+//			fn.call(obj,ev,down);
+			fn.apply(obj,[ev,down]);
+			if (ev.preventDefault){
+				ev.preventDefault();
+			}
+			return false;
+		}
+	}
